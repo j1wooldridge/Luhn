bbf8c971aa69ecf2c2441ba21af49d284b59d28d
Jacques Wooldridge <jacques.wooldridge@gmail.com>
2020-01-09 15:14:10 +0000

    new updates made up until 2020-01-09 09:20

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,5 +1,27 @@
 module Luhn
-  def self.is_valid?(number)
-    #WRITE YOUR SOLUTION HERE
+  def self.is_valid?(ccard_number)
+
+    str_nums = ccard_number.to_s.split(//)  #Step 1 - break the credit card into its individual digits.
+
+    nums = Array.new
+
+    str_nums.each do |str|
+      nums << str.to_i  
+    end
+
+    nums.reverse!   #Step 2 - Start from the right
+
+    sum = 0
+
+    nums.each.with_index(1) do |n, i|    #Step 2 - Double every second digit (i.e., digit 2, 4, 6 etc.). 
+      if i % 2 == 0    
+        d = n * 2>9 ? n *2-9 : n *2     #Step 3 - If the doubled value is greater than or equal to 10, take the value and subtract 9 from it.
+      else
+        d = n
+      end
+      sum += d  #Step 4 - Sum the digits.
+    end
+    (sum % 10) == 0   #Step 5 - If the sum is divisible by 10, it's a valid number. Otherwise it's invalid
+    
   end
 end
